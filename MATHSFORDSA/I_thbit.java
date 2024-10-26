d8343b16446ef111c52920028a7cd0baf93487b9
biswajit0123 <biswajitmuduli0544@gmail.com>
2024-10-26 21:39:30 +0530

    all

--- MATHSFORDSA/I_thbit.java
+++ MATHSFORDSA/I_thbit.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class I_thbit {
+
+
+// find the ith bit of a number
+public class I_thbit {
+    public static void main(String[] args) {
+        int num = 11;
+        System.out.println(findBit(num,1));
+    }
+
+    static int findBit(int n,int pos){
+        return  n & (1 << (pos-1));
+    }
 }
